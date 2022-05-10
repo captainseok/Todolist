0ccf9cf552910640a5b4dcd1d8327cac76a11f07
captainseok <102131562+captainseok@users.noreply.github.com>
2022-05-10 16:45:02 +0900

    Add files via upload

--- explain.js
+++ explain.js
@@ -1,6 +1,8 @@
 const memo = document.getElementById('press');
 const displayMemo = document.querySelector('.quest__content');
+const allContents = document.querySelector('#contents');
 
 memo.addEventListener('click',() => {
     displayMemo.classList.toggle('active');
+    allContents.classList.toggle('active');
 });
